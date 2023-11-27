91aa6d3294004289e24d50aeac1985c2cb3b777a
reb <nivokazi21@itu.edu.tr>
2023-11-27 16:40:33 +0300

    fixed the home page
    There is still room for improvement, until now this is the version. The project runs from main.py file.

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from flask import Flask
 
 import views
+from views import index_page, configure_app
 
+# ... your existing imports ...
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("settings")
 
-    app.add_url_rule("/", view_func=views.index_page)
+    configure_app(app)
+    app.add_url_rule("/", view_func=views.index_page, methods=['GET'])
     app.add_url_rule("/game_plays", view_func=views.game_plays)
     app.add_url_rule("/game_goalie_stats", view_func=views.game_goalie_stats)
     app.add_url_rule("/game_skater_stats", view_func=views.game_skater_stats)
     app.add_url_rule("/game_teams_stats", view_func=views.game_teams_stats)
 
+    
+    app.add_url_rule("/upload_goalie_stats", view_func=views.upload_goalie_stats, methods=['POST'])
+    app.add_url_rule("/delete_goalie_stats", view_func=views.delete_goalie_stats, methods=['POST'])
 
     return app
 
