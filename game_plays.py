f8986992ab203a7f1e7683569497ad5dcca7682b
Gülşah Yorulmaz <yrlmzglsh@gmail.com>
2023-12-01 20:44:09 +0300

    Add database connection and dataset for game_plays
    Add config.py to .gitignore to avoid database password conflict
    Make small changes to style.css
    Fix base.html and index.html
    Change game_plays.html according to the new configuration
    Fix image problems for the pages in views.py

---
+++ game_plays.py
@@ -0,0 +1,91 @@
+import mysql.connector
+import pandas as pd
+import numpy as np
+from config import MYSQL_CONFIG
+
+# connection details
+mydb = mysql.connector.connect(**MYSQL_CONFIG)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("DROP TABLE IF EXISTS game_plays")
+
+# query for creating the game_plays table
+create_game_plays_table = '''
+    CREATE TABLE game_plays(
+        play_id VARCHAR(15) NOT NULL PRIMARY KEY,
+        game_id INT NOT NULL,
+        team_id_for INT,
+        team_id_against INT,
+        event VARCHAR(255),
+        secondary_type VARCHAR(255),
+        x INT,
+        y INT,
+        period INT,
+        period_type VARCHAR(255),
+        period_time INT,
+        period_time_remaining INT,
+        date_time DATETIME,
+        goals_away INT,
+        goals_home INT,
+        description VARCHAR(255),
+        st_x INT,
+        st_y INT
+    )
+'''
+
+mycursor.execute(create_game_plays_table)
+
+# read the data file
+csv_file = pd.read_csv('nhl-db/game_plays.csv', usecols=[
+    'play_id',
+    'game_id',
+    'team_id_for',
+    'team_id_against',
+    'event',
+    'secondaryType',
+    'x',
+    'y',
+    'period',
+    'periodType',
+    'periodTime',
+    'periodTimeRemaining',
+    'dateTime',
+    'goals_away',
+    'goals_home',
+    'description',
+    'st_x',
+    'st_y'
+])
+
+csv_file.replace(['', 'NA'], None, inplace=True)
+csv_file = csv_file.where(pd.notna(csv_file), None)
+data_to_insert = csv_file.to_records(index=False).tolist()
+data_to_insert = [tuple(None if pd.isna(value) else value for value in row) for row in data_to_insert]
+
+insert_query = '''
+    INSERT INTO game_plays
+    (play_id,
+    game_id,
+    team_id_for,
+    team_id_against,
+    event,
+    secondary_type,
+    x,
+    y,
+    period,
+    period_type,
+    period_time,
+    period_time_remaining,
+    date_time,
+    goals_away,
+    goals_home,
+    description,
+    st_x,
+    st_y)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+'''
+
+mycursor.executemany(insert_query, data_to_insert)
+
+mydb.commit()
--- views.py
+++ views.py
@@ -12,16 +12,35 @@ def configure_app(app):
 app = Flask(__name__)
 configure_app(app)
 
-def index_page():
-    img_path = '05_NHL_Shield.svg.webp' 
-    img_path2 = 'youtube_logo.png' 
-    full_img_path = os.path.join(current_app.config['UPLOAD_FOLDER'], img_path)
-    full_img_path2 = os.path.join(current_app.config['UPLOAD_FOLDER'], img_path2)
+def generate_image_paths():
+    img_logo_path = '05_NHL_Shield.svg.webp' 
+    img_youtube_path = 'youtube_logo.png'
+    img_x_path = 'x_logo.png'
+    img_instagram_path = 'instagram_logo.png'
+    img_facebook_path = 'facebook_logo.png'
+    
+    full_img_path1 = os.path.join(current_app.config['UPLOAD_FOLDER'], img_logo_path)
+    full_img_path2 = os.path.join(current_app.config['UPLOAD_FOLDER'], img_youtube_path)
+    full_img_path3 = os.path.join(current_app.config['UPLOAD_FOLDER'], img_x_path)
+    full_img_path4 = os.path.join(current_app.config['UPLOAD_FOLDER'], img_instagram_path)
+    full_img_path5 = os.path.join(current_app.config['UPLOAD_FOLDER'], img_facebook_path)
+    
+    return {
+        'icon': full_img_path1,
+        'nhl_logo': full_img_path1,
+        'youtube': full_img_path2,
+        'x': full_img_path3,
+        'instagram': full_img_path4,
+        'facebook': full_img_path5,
+    }
 
-    return render_template('index.html', user_image=full_img_path, youtube=full_img_path2)
+def index_page():
+    image_paths = generate_image_paths()
+    return render_template('index.html', **image_paths, active_page = 'index')
 
 def game_plays():
-    return render_template("game_plays.html")
+    image_paths = generate_image_paths()
+    return render_template("game_plays.html", **image_paths, active_page = 'game_plays')
 
 def game_skater_stats():
     return render_template("game_skater_stats.html")
