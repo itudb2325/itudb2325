596d280c0355dee910f7eb145773d6808d44d80d
SelinDurmus16 <durmusselin8@gmail.com>
2023-12-03 18:31:32 +0300

    wrote the inital code for connections and table creations

--- game_skater_stats.py
+++ game_skater_stats.py
@@ -1,3 +1,22 @@
 import mysql.connector
 import pandas as pd
+import numpy as np
 from config import MYSQL_CONFIG
+
+#we need to make a connection
+mydb = mysql.connector.connect(**MYSQL_CONFIG)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("DROP TABLE IF EXISTS game_skater_stats")
+
+create_game_skater_stats_table = '''
+    CREATE table game_skater_stats(
+    
+
+
+    )
+
+
+
+'''  
