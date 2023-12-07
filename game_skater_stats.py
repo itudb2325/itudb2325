f2fb08ccb23f4133138fee081a45315c75f1a1b7
SelinDurmus16 <durmusselin8@gmail.com>
2023-12-07 15:43:18 +0300

    created the tables and set the types of the columns

--- game_skater_stats.py
+++ game_skater_stats.py
@@ -12,7 +12,30 @@ mycursor.execute("DROP TABLE IF EXISTS game_skater_stats")
 
 create_game_skater_stats_table = '''
     CREATE table game_skater_stats(
-    
+      
+        game_id INT,
+        player_id INT NOT NULL, 
+        team_id INT,
+        timeOnIce INT,
+        assists INT,
+        goals INT,
+        shots INT,
+        hits INT,
+        powerPlayGoals INT,
+        powerPlayAssists INT,
+        penaltyMinutes INT,
+        faceOffWins INT,
+        faceoffTaken INT,
+        takeaways INT,
+        giveaways INT,
+        shortHandedGoals INT,
+        shortHandedAssists INT,
+        blocked INT,
+        plusMinus INT,
+        evenTimeOnIce INT,
+        shortHandedTimeOnIce INT,
+        powerPlayTimeOnIce INT
+
 
 
     )
@@ -20,3 +43,73 @@ create_game_skater_stats_table = '''
 
 
 '''  
+
+mycursor.execute(create_game_skater_stats_table)
+
+csv_file= pd.read_csv('nhl-db/game_skater_stats.csv', usecols=[
+         
+        'game_id',
+        'player_id',
+        'team_id',
+        'timeOnIce',
+        'assists',
+        'goals',
+        'shots',
+        'hits',
+        'powerPlayGoals',
+        'powerPlayAssists',
+        'penaltyMinutes',
+        'faceOffWins',
+        'faceoffTaken',
+        'takeaways',
+        'giveaways',
+        'shortHandedGoals',
+        'shortHandedAssists',
+        'blocked',
+        'plusMinus',
+        'evenTimeOnIce',
+        'shortHandedTimeOnIce',
+        'powerPlayTimeOnIce'
+
+])
+
+csv_file.replace(['', 'NA'], None, inplace=True)
+csv_file = csv_file.where(pd.notna(csv_file), None)
+data_to_insert = csv_file.to_records(index=False).tolist()
+data_to_insert = [tuple(None if pd.isna(value) else value for value in row) for row in data_to_insert]
+
+
+
+insert_query = '''
+    INSERT INTO game_skater_stats(
+
+    game_id ,
+    player_id , 
+    team_id ,
+    timeOnIce ,
+    assists ,
+    goals ,
+    shots ,
+    hits ,
+    powerPlayGoals ,
+    powerPlayAssists ,
+    penaltyMinutes ,
+    faceOffWins ,
+    faceoffTaken ,
+    takeaways ,
+    giveaways ,
+    shortHandedGoals ,
+    shortHandedAssists ,
+    blocked ,
+    plusMinus ,
+    evenTimeOnIce ,
+    shortHandedTimeOnIce ,
+    powerPlayTimeOnIce )
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+
+
+'''
+
+mycursor.executemany(insert_query, data_to_insert)
+
+mydb.commit()
